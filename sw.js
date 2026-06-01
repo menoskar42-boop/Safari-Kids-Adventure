df9a602baa0720b11afc082799986d17953ca9c2
Safari Kids Dev <menoskar42@gmail.com>
2026-06-01 09:44:29 +0000

    SEO: enrich root head (canonical/OG/Twitter/JSON-LD WebApplication) + og.svg preview image. SW v119

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== Service Worker: تخزين مؤقت للعمل دون اتصال =====
-const CACHE = "safari-kids-v118";
+const CACHE = "safari-kids-v119";
 const ASSETS = [
   "/app",
   "/manifest.webmanifest",
